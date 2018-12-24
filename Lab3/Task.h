e0e34c025a95faa1369e37d135ddbe0b6c114572
Oleh Shkalikov <Shkalikov.Oleh@outlook.com>
2018-12-24 20:11:30 +0200

    Separate labs(2)

--- Lab3/Task.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-bool task1_Check(double, double);
-
-double task1_Calculate(double, double);
-
-void add_task(int);
--- Lab5/Task.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <unordered_map>
-#include <string>
-
-bool sum_base_task(const char*);
-
-bool dump8_medium_task(const char*);
-
-std::unordered_map<std::string, int> bigramm_hard_task(const char*);
