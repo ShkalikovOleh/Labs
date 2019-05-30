20b856655477cf48ee950583f4c838b0d822f906
Oleh Shkalikov <Shkalikov.Oleh@outlook.com>
2019-05-30 18:17:31 +0300

    Pass Lab6

--- part 2/Lab6/Lab6.cpp
+++ part 2/Lab6/Lab6.cpp
@@ -4,7 +4,7 @@
 #pragma hdrstop
 #include <tchar.h>
 //---------------------------------------------------------------------------
-USEFORM("MainWindow.cpp", Form1);
+USEFORM("MainForm.cpp", Form1);
 //---------------------------------------------------------------------------
 int WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
 {
