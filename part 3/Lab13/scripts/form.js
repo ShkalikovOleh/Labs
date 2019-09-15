1997cd4f9bc927010e7d7d6c7aab54889b8ec3da
Oleh Shkalikov <Shkalikov.Oleh@outlook.com>
2019-09-15 19:48:45 +0300

    Validation fix

--- part 3/Lab13/scripts/form.js
+++ part 3/Lab13/scripts/form.js
@@ -3,27 +3,34 @@ var lastInput;
 
 document.addEventListener("DOMContentLoaded", function() {
     
-    modal = document.getElementsByClassName('modal')[0];
-    modal.style.visibility = 'hidden';
+    $(".modal").hide();    
     lastInput = Date.now();
-    setInterval(ShowPopup, 60000)
-
-    $("form").validate();
+    setInterval(ShowPopup, 60000);
+    
     $.validator.addMethod(
         "regex",
         function(value, element, regexp) {
-            var re = new RegExp(regexp);
-            return re.test(value);
+            return this.optional(element) || regexp.test(value);
         },
-        alert("Name can not contains non latin letter.")
-    );
-    $("#Name").rules("add", { regex: "^[A-Za-z]+$" })
+        "Incorrect pattern"
+    );    
+    $("form").validate(
+    {
+        rules:
+        {
+            Name:
+            {
+                required: true,
+                regex: /^[A-Za-z\s]+$/
+            }
+        }            
+    });
 });
 
 function SubscribeOnInputs()
 {
     var inputs, index;
-
+    
     inputs = document.getElementsByTagName('input');
     for (index = 0; index < inputs.length; ++index) {
         inputs[index].oninput = function()
@@ -37,26 +44,25 @@ function ShowPopup()
 {
     var delta = (Date.now() - lastInput) * 1000 * 60;
     if(delta > 1)
-        modal.style.visibility = 'visible';
+        $('.modal').show();
 }
 
 function ClosePopup()
 {
-    modal.style.visibility = 'hidden';
+    $(".modal").hide();
     lastInput = Date.now();
 }
 
 function ValidateForm()
 {
-    var name = document.forms[0]['Name'].value;
+    var name = document.getElementById('Name').value;
     var re = new RegExp(/^[A-Za-z]+$/);
     if(re.test(name))
     {
         return true;
     }
     else
-    {
-        alert("Name can not contains non latin letter");
+    {        
         return false;
     }
 }
