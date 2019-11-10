60236a7f3abd8790288de633f3973786e47a4f09
Oleh Shkalikov <Shkalikov.Oleh@outlook.com>
2019-11-10 15:54:40 +0200

    Add procent regulation for VHI year range

--- part 3/Ukraine VHI/VHITool.py
+++ part 3/Ukraine VHI/VHITool.py
@@ -9,8 +9,15 @@ def getVHISeriesByProvince(df, provinceId):
     return df[df.Province == provinceId].VHI
 
 
-def getYearInVHIRangeByProvince(df, provinceId, min, max):
-    return df[(df.Province == provinceId) & (df.VHI >= min) & (df.VHI <= max)].year.unique()
+def getYearInVHIRangeByProvince(df, provinceId, min, max, procentByYear):
+    years = df[(df.Province == provinceId)].year.unique()
+    results = []
+    for year in years:
+        series = getVHISeriesByProvinceAndYear(df, provinceId, year)
+        percent = series[(series >= min) & (series <= max)].count() * 100 / series.count()
+        if(percent >= procentByYear):
+            results.append(year)
+    return results
 
 
 df = utils.uploadDataFromDirectory("data")
@@ -20,5 +27,5 @@ print("Mean of 1 in 2017: {}".format(series.mean()))
 print("Min of 1 in 2017: {}".format(series.min()))
 print("Max of 1 in 2017: {}".format(series.max()))
 
-print("Medium in 1: {}".format(getYearInVHIRangeByProvince(df,1,15,35)))
-print("Critical in 1: {}".format(getYearInVHIRangeByProvince(df,1,0,15)))
+print("Medium in 1: {}".format(getYearInVHIRangeByProvince(df,1,15,35,30)))
+print("Critical in 1: {}".format(getYearInVHIRangeByProvince(df,1,0,15,30)))
