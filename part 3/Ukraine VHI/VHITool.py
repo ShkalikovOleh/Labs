1ec20552bc95d89b678c079921cc36a90aa95126
Oleh Shkalikov <Shkalikov.Oleh@outlook.com>
2019-11-22 21:39:50 +0200

    Add null counter

--- part 3/Ukraine VHI/VHITool.py
+++ part 3/Ukraine VHI/VHITool.py
@@ -22,6 +22,11 @@ def getYearInVHIRangeByProvince(df, provinceId, min, max, procentByYear):
 
 df = utils.uploadDataFromDirectory("data")
 
+count = df.isna().any().count()
+for i in range(1991,2019):
+    count += 52*25 - df[(df.year == i)].week.count()
+print(count)
+
 series = getVHISeriesByProvinceAndYear(df,1,2017)
 print("Mean of 1 in 2017: {}".format(series.mean()))
 print("Min of 1 in 2017: {}".format(series.min()))
