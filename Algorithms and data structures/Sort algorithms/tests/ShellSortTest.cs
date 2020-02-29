8266f9fdf521c9aafc99b6937302da4126bb1d0f
Oleh Shkalikov <Shkalikov.Oleh@outlook.com>
2020-02-29 11:43:16 +0200

    Add ShellSort correctness tests

--- Algorithms and data structures/Sort algorithms/tests/ShellSortTest.cs
+++ Algorithms and data structures/Sort algorithms/tests/ShellSortTest.cs
@@ -6,8 +6,10 @@ namespace tests
     public class ShellSortTest
     {
         [Theory]
-        [InlineData(10)]        
-        public void CorrectPartialInsertionSortTest(uint count)
+        [InlineData(10)]
+        [InlineData(100)]
+        [InlineData(1000)]
+        public void ShellSortShellSequenceCorrectTest(int count)
         {
             Random random = new Random();
 
@@ -17,7 +19,44 @@ namespace tests
                 array[i] = random.Next(-100,100);
             }
                         
-            var result = array.PartialInsertionSort(3);
+            var result = array.ShellSort(new ShellSequence());
+            Assert.Equal(0, result.CountInversion());
+        }
+
+        [Theory]
+        [InlineData(10)]
+        [InlineData(100)]
+        [InlineData(1000)]
+        public void ShellSortHibbardSequenceCorrectTest(int count)
+        {
+            Random random = new Random();
+
+            var array = new int[count];
+            for(int i = 0; i < count; i++)
+            {
+                array[i] = random.Next(-100,100);
+            }
+                        
+            var result = array.ShellSort(new HibbardSequence());
+            Assert.Equal(0, result.CountInversion());
+        }
+
+        [Theory]
+        [InlineData(10)]
+        [InlineData(100)]
+        [InlineData(1000)]
+        public void ShellSortKnuthSequenceCorrectTest(int count)
+        {
+            Random random = new Random();
+
+            var array = new int[count];
+            for(int i = 0; i < count; i++)
+            {
+                array[i] = random.Next(-100,100);
+            }
+                        
+            var result = array.ShellSort(new KnuthSequence());
+            Assert.Equal(0, result.CountInversion());
         }
     }
 }
