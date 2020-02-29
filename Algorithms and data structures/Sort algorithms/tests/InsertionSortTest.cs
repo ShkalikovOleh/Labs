cdc88d231459030816bd7d0adfcce41e432d6c8b
Oleh Shkalikov <Shkalikov.Oleh@outlook.com>
2020-02-29 11:43:16 +0200

    Add ShellSort correctness tests

--- Algorithms and data structures/Sort algorithms/tests/InsertionSortTest.cs
+++ Algorithms and data structures/Sort algorithms/tests/InsertionSortTest.cs
@@ -20,6 +20,22 @@ namespace tests
             }
                         
             var result = array.InsertionSort();
+            Assert.Equal(0, result.CountInversion());
+        }
+
+        [Theory]
+        [InlineData(10, 3)]        
+        public void CorrectPartialInsertionSortTest(uint count, int step)
+        {
+            Random random = new Random();
+
+            var array = new int[count];
+            for(int i = 0; i < count; i++)
+            {
+                array[i] = random.Next(-100,100);
+            }
+
+            var result = array.PartialInsertionSort(step);
         }
     }
 }
