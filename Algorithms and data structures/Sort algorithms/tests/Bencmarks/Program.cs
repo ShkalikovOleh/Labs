c47e75578e4188e938cecceb2d79e0056e8f7398
Oleh Shkalikov <Shkalikov.Oleh@outlook.com>
2020-03-01 14:06:40 +0200

    Add benchmark switcher

---
+++ Algorithms and data structures/Sort algorithms/tests/Bencmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Bencmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+        }
+    }
+}
