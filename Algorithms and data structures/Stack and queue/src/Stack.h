81c5dda246cf239345f1b8495bac098eb2dfd579
Oleh Shkalikov <Shkalikov.Oleh@outlook.com>
2020-03-18 19:15:21 +0200

    Add Stack

---
+++ Algorithms and data structures/Stack and queue/src/Stack.h
@@ -0,0 +1,28 @@
+#ifndef STACK_H
+#define STACK_H
+
+template <typename T, typename Container>
+class Stack
+{
+private:
+    Container _container;
+
+public:
+    Stack(std::initializer_list<T>);
+    Stack(const Container& container);
+    Stack(const Stack& container);
+
+    bool empty() const noexcept;
+    std::size_t size() const noexcept;
+
+    T& top();
+    const T& top() const;    
+
+    void push(T&&);
+    void push(const T&);
+    void pop();  
+};
+
+#include "Stack.cpp"
+
+#endif
