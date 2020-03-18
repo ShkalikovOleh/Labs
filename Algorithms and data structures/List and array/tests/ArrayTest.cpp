e3b144de8600472e701d1561886f1fc8ec5ccb84
Oleh Shkalikov <Shkalikov.Oleh@outlook.com>
2020-03-18 11:23:23 +0200

    Add Array tests

--- Algorithms and data structures/List and array/tests/ArrayTest.cpp
+++ Algorithms and data structures/List and array/tests/ArrayTest.cpp
@@ -7,29 +7,152 @@ class ArrayTest: public ::testing::Test, public Array<int>
 protected:
     void SetUp()
     {
-        push_back(4);
+        push_back(66);
         push_back(5);
-        push_back(6);
-    };
-    void TearDown(){    };
+        push_back(66);
+    };    
 };
+
+TEST_F(ArrayTest, shrinkTest)
+{
+    int first = _data[0];
+    int last = _data[_size - 1];
+
+    shrinkToFit();
+
+    EXPECT_EQ(size(), capacity());    
+    EXPECT_EQ(first, _data[0]);
+    EXPECT_EQ(last, _data[_size - 1]);
+}
+
+TEST_F(ArrayTest, frontTest)
+{
+    ASSERT_EQ(_data[0], front());
+}
+
+TEST_F(ArrayTest, frontThrowIfEmptyTest)
+{
+    Array<int> test;
+    ASSERT_THROW(test.front(), std::length_error);
+}
+
+TEST_F(ArrayTest, backTest)
+{
+    ASSERT_EQ(_data[_size - 1], back());
+}
+
+TEST_F(ArrayTest, backThrowIfEmptyTest)
+{
+    Array<int> test;
+    ASSERT_THROW(test.back(), std::length_error);
+}
 
 TEST_F(ArrayTest, pushBackTest)
 {
+    int pr_size = size();
+    
     push_back(4);
-    ASSERT_EQ(4, _data[_size - 1]);
+    
+    EXPECT_EQ(4, _data[_size - 1]);
+    EXPECT_EQ(++pr_size, size());
+}
+
+TEST_F(ArrayTest, pushFrontTest)
+{
+    int pr_size = size();
+
+    push_front(-1);
+
+    EXPECT_EQ(-1, _data[0]);
+    EXPECT_EQ(++pr_size, size());
+}
+
+TEST_F(ArrayTest, popBackTest)
+{
+    int pr_size = size();
+    int prelast = _data[_size - 2];
+
+    pop_back();
+
+    EXPECT_EQ(--pr_size, size());
+    EXPECT_EQ(prelast, _data[_size - 1]);
+}
+
+TEST_F(ArrayTest, popFrontTest)
+{
+    int pr_size = size();
+    int pastFirst = _data[1];
+
+    pop_front();
+
+    EXPECT_EQ(--pr_size, size());
+    EXPECT_EQ(pastFirst, _data[0]);
 }
 
 TEST_F(ArrayTest, insertEndTest)
 {
+    int prSize = _size;
+
     insert(cend() - 1, 11);
-    ASSERT_EQ(11, _data[_size - 1]);
+    EXPECT_EQ(11, _data[_size - 1]);
+    EXPECT_EQ(++prSize, _size);
 }
 
 TEST_F(ArrayTest, insertMiddleTest)
 {
+    int prSize = _size;
+    int first = _data[0];
+    int last = _data[_size - 1];
+
     insert(cbegin() + 1, 12);
-    ASSERT_EQ(12, _data[2]);
+
+    EXPECT_EQ(12, _data[2]);
+    EXPECT_EQ(++prSize, _size);
+    EXPECT_EQ(first, _data[0]);
+    EXPECT_EQ(last, _data[_size - 1]);
+}
+
+TEST_F(ArrayTest, removeByValueTest)
+{
+    remove(66);
+
+    bool flag = false;
+    for(int i = 0; i < _size; i++)
+    {
+        if(_data[i] == 66)
+        {
+            flag = true;
+            break;
+        }            
+    }        
+    ASSERT_FALSE(flag);
+}
+
+TEST_F(ArrayTest, removeByIteratorTest)
+{
+    int first = _data[0];
+    int last = _data[_size - 1];
+    int removed = _data[1];
+    int prSize = _size;
+
+    remove(cbegin() + 1);
+
+    EXPECT_NE(removed, _data[1]);
+    EXPECT_EQ(--prSize, _size);
+    EXPECT_EQ(first, _data[0]);
+    EXPECT_EQ(last, _data[_size - 1]);
+}
+
+TEST_F(ArrayTest, removeByIteratorRangeTest)
+{
+    int first = _data[0];
+    int last = _data[_size - 1];        
+
+    remove(cbegin() + 1, cend() - 2);
+    
+    EXPECT_EQ(2, _size);
+    EXPECT_EQ(first, _data[0]);
+    EXPECT_EQ(last, _data[_size - 1]);
 }
 
 TEST_F(ArrayTest, indexingTest)
@@ -37,7 +160,7 @@ TEST_F(ArrayTest, indexingTest)
     ASSERT_EQ(_data[2], operator[](2));
 }
 
-TEST_F(ArrayTest, iterationsTest)
+TEST_F(ArrayTest, iterationTest)
 {
     int i = 0;
     for(int& value : *this)
